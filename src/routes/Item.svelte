dd8946ace3b954efcea27b83c1367b535efa58ab
stef-the <stefthedoggo4@gmail.com>
2022-03-03 18:01:07 -0800

    update

--- src/routes/Item.svelte
+++ src/routes/Item.svelte
@@ -1,38 +1,31 @@
 <script>
 	// @ts-nocheck
-	// import { onMount } from 'svelte'; // might use later
-
-	//export let items;
-
 	import duplicates from '../../static/data/duplicates.json';
-	console.log(duplicates);
 
 	export let out = { displayname: '' };
 	export let out2 = {};
-
 	export const exclusions = [];
-
 	export const inputs = [
 		{
 			name: 'iname',
 			title: 'Internal Name',
-			placeholder: 'END_SWORD',
+			placeholder: 'END_SWORD'
 		},
 		{
 			name: 'txname',
 			title: 'Texture Name',
 			placeholder: 'texture.png',
-			split: true
+			split: true // broken atm
 		},
 		{
 			name: 'fname',
 			title: 'File Name',
-			placeholder: 'texture.properties',
+			placeholder: 'texture.properties'
 		},
 		{
 			name: 'jname',
 			title: 'JSON Name',
-			placeholder: 'model.json',
+			placeholder: 'model.json'
 		}
 	];
 
@@ -69,46 +62,8 @@
 		const response = await fetch(input, {
 			method: 'GET'
 		});
-		// console.log('Status Code [', response.status, ']') // Sends response status code
-		// console.log(await response.clone().text())
 		return response.json();
 	}
-
-	/*
-
-	// Retrieve all items from NEU Item Repository
-	async function findItems() {
-		const data = await getJSON(
-			`https://api.github.com/repos/NotEnoughUpdates/NotEnoughUpdates-REPO/contents/items?ref=master`
-		);
-
-		// console.log(data);
-
-		let items = { items: [], data: [], displaynames: [] };
-
-		for (const item of data) {
-			const result = await getJSON(item['download_url']);
-			items.data.push(result);
-			items.displaynames.push(displayRename(result['displayname']));
-			items.items.push(item['name']);
-		}
-		return items;
-	}
-
-	findItems().then(
-		function (res) {
-			items = res;
-			console.log(items);
-		},
-		function (err) {
-			console.error(err);
-			alert(err);
-		}
-	).catch((err) => {
-		console.error(err);
-	});
-
-	*/
 </script>
 
 <title>CIT Generator - Items</title>
@@ -120,7 +75,7 @@
 <form autocomplete="off">
 	<div class="labelcontainer">
 		{#each inputs as item}
-			<div class="labelgroup">
+			<div class="labelgroup" id="{item.name}container">
 				<label for={item.name}>{item.title}</label>
 				<div class="linebreak" />
 				<input
@@ -132,7 +87,7 @@
 				/><br />
 			</div>
 			{#if item.split}
-				<div class="split"></div>
+				<div class="split" />
 			{/if}
 		{/each}
 	</div>
@@ -143,48 +98,54 @@
 			type="button"
 			name="submit"
 			value="Submit"
-			on:click={() =>
-				getJSON(
-					`https://raw.githubusercontent.com/NotEnoughUpdates/NotEnoughUpdates-REPO/master/items/${
-						document.getElementById('iname').value
-					}.json`
-				).then((result) => {
-					out = result;
-					let re;
-	
-
-					if (displayRename(result.displayname) in duplicates.data) { // if the item is excluded from standard CIT
-						re = `type=item\nitems=${result.itemid}\nnbt.ExtraAttributes.id=${
+			on:click={function submit() {
+				if (document.getElementById('iname').value) {
+					getJSON(
+						`https://raw.githubusercontent.com/NotEnoughUpdates/NotEnoughUpdates-REPO/master/items/${
 							document.getElementById('iname').value
-						}`;
-					} else { // if the item is included in standard CIT
-						re = `type=item\nitems=${
-							result.itemid
-						}\nnbt.display.Name=ipattern:*${
-							document.getElementById('iname').value
-						}*`;
-					} if (document.getElementById('txname').value) { // if texture name is given
-						re += `\ntexture=${
-							document.getElementById('txname').value
-						}`
-					} if (document.getElementById('jname').value) { // if json name is given
-						re += `\nmodel=./${
-							document.getElementById('jname').value
-						}`
-					}
+						}.json`
+					).then((result) => {
+						out = result;
+						let re;
 
-					out2 = {
-						// output for file editor / download
-						content: re,
-						fname: document.getElementById('fname').value || document.getElementById('iname').value
-					};
+						if (displayRename(result.displayname) in duplicates.data) {
+							// if the item is excluded from standard CIT
+							re = `type=item\nitems=${result.itemid}\nnbt.ExtraAttributes.id=${
+								document.getElementById('iname').value
+							}`;
+						} else {
+							// if the item is included in standard CIT
+							re = `type=item\nitems=${result.itemid}\nnbt.display.Name=ipattern:*${
+								document.getElementById('iname').value
+							}*`;
+						}
+						if (document.getElementById('txname').value) {
+							// if texture name is given
+							re += `\ntexture=${document.getElementById('txname').value}`;
+						}
+						if (document.getElementById('jname').value) {
+							// if json name is given
+							re += `\nmodel=./${document.getElementById('jname').value}`;
+						}
 
-					console.log(out2);
+						out2 = {
+							// output for file editor / download
+							content: re,
+							fname:
+								document.getElementById('fname').value || document.getElementById('iname').value
+						};
 
-					// unhide download button
-					document.getElementById('downloadcontainer').classList.remove('hidden');
-					document.getElementById('downloadbutton').classList.remove('hidden');
-				})}
+						console.log(out2);
+
+						// unhide download button
+						document.getElementById('downloadcontainer').classList.remove('hidden');
+						document.getElementById('downloadbutton').classList.remove('hidden');
+						document.getElementById('inamecontainer').classList.remove('required')
+					});
+				} else {
+					document.getElementById('inamecontainer').classList.add('required')
+				}
+			}}
 		/>
 	</div>
 	<div class="linebreak" />
@@ -203,3 +164,4 @@
 		/>
 	</div>
 </form>
+<div id="alert"><div><span></span></div></div>
