9f4ee1e9d3a6d75057d3fc3593a859abbd2f855b
stef-the <stefthedoggo4@gmail.com>
2022-03-03 21:12:15 -0800

    pog

--- src/routes/Item.svelte
+++ src/routes/Item.svelte
@@ -1,5 +1,6 @@
 <script>
 	// @ts-nocheck
+	import { exclude_internal_props } from 'svelte/internal';
 	import duplicates from '../../static/data/duplicates.json';
 
 	export let out = { displayname: '' };
@@ -64,6 +65,24 @@
 		});
 		return response.json();
 	}
+
+	function beep(error) {
+		console.log('beep');
+		const a = error.replace('#code', '<span class="code">').replace('/code', '</span>');
+		document.getElementById('inamecontainer').classList.add('required');
+		document.getElementById('alert').classList.remove('hidden');
+		document.getElementById('alertbox').classList.remove('hidden');
+		document.getElementById(
+			'spancontainer'
+		).innerHTML = `<span>${a}</span><div style="padding: 1rem;"></div>`;
+	}
+
+	function boop() {
+		console.log('boop');
+		document.getElementById('alert').classList.add('hidden');
+		document.getElementById('alertbox').classList.add('hidden');
+		document.getElementById('spancontainer').innerHTML = '';
+	}
 </script>
 
 <title>CIT Generator - Items</title>
@@ -72,7 +91,7 @@
 	<a class="linkto" href="/">Item CIT Generator</a>
 </h1>
 <div class="linebreak" />
-<form autocomplete="off">
+<form autocomplete="off" class="main">
 	<div class="labelcontainer">
 		{#each inputs as item}
 			<div class="labelgroup" id="{item.name}container">
@@ -98,52 +117,58 @@
 			type="button"
 			name="submit"
 			value="Submit"
-			on:click={function submit() {
+			on:click={function () {
 				if (document.getElementById('iname').value) {
 					getJSON(
 						`https://raw.githubusercontent.com/NotEnoughUpdates/NotEnoughUpdates-REPO/master/items/${
 							document.getElementById('iname').value
 						}.json`
-					).then((result) => {
-						out = result;
-						let re;
+					)
+						.then((result) => {
+							out = result;
+							let re;
 
-						if (displayRename(result.displayname) in duplicates.data) {
-							// if the item is excluded from standard CIT
-							re = `type=item\nitems=${result.itemid}\nnbt.ExtraAttributes.id=${
-								document.getElementById('iname').value
-							}`;
-						} else {
-							// if the item is included in standard CIT
-							re = `type=item\nitems=${result.itemid}\nnbt.display.Name=ipattern:*${
-								document.getElementById('iname').value
-							}*`;
-						}
-						if (document.getElementById('txname').value) {
-							// if texture name is given
-							re += `\ntexture=${document.getElementById('txname').value}`;
-						}
-						if (document.getElementById('jname').value) {
-							// if json name is given
-							re += `\nmodel=./${document.getElementById('jname').value}`;
-						}
+							if (displayRename(result.displayname) in duplicates.data) {
+								// if the item is excluded from standard CIT
+								re = `type=item\nitems=${result.itemid}\nnbt.ExtraAttributes.id=${
+									document.getElementById('iname').value
+								}`;
+							} else {
+								// if the item is included in standard CIT
+								re = `type=item\nitems=${result.itemid}\nnbt.display.Name=ipattern:*${
+									document.getElementById('iname').value
+								}*`;
+							}
+							if (document.getElementById('txname').value) {
+								// if texture name is given
+								re += `\ntexture=${document.getElementById('txname').value}`;
+							}
+							if (document.getElementById('jname').value) {
+								// if json name is given
+								re += `\nmodel=./${document.getElementById('jname').value}`;
+							}
 
-						out2 = {
-							// output for file editor / download
-							content: re,
-							fname:
-								document.getElementById('fname').value || document.getElementById('iname').value
-						};
+							out2 = {
+								// output for file editor / download
+								content: re,
+								fname:
+									document.getElementById('fname').value || document.getElementById('iname').value
+							};
 
-						console.log(out2);
+							console.log(out2);
 
-						// unhide download button
-						document.getElementById('downloadcontainer').classList.remove('hidden');
-						document.getElementById('downloadbutton').classList.remove('hidden');
-						document.getElementById('inamecontainer').classList.remove('required')
-					});
+							// unhide download button
+							document.getElementById('downloadcontainer').classList.remove('hidden');
+							document.getElementById('downloadbutton').classList.remove('hidden');
+							document.getElementById('inamecontainer').classList.remove('required');
+						})
+						.catch(function () {
+							beep(
+								'Input #codeInternal Name/code needs to have a valid value<br>(a Hypixel Skyblock item ID).'
+							);
+						});
 				} else {
-					document.getElementById('inamecontainer').classList.add('required')
+					beep('Input #codeInternal Name/code needs to have a valid value.');
 				}
 			}}
 		/>
@@ -159,9 +184,23 @@
 			value="Download"
 			on:click={() => {
 				download(out2.content, out2.fname, 'text');
-				window.location.reload(); // reload page
+				window.location.reload();
 			}}
 		/>
 	</div>
 </form>
-<div id="alert"><div><span></span></div></div>
+<div id="alert" class="hidden" on:click={() => boop()}>
+	<div id="alertbox" class="hidden">
+		<div id="spancontainer" />
+		<input
+			class="enter"
+			id="ok"
+			type="button"
+			name="ok"
+			value="OK"
+			on:click={() => {
+				console.log('hello!');
+			}}
+		/>
+	</div>
+</div>
